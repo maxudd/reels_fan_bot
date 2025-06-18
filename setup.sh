b1d84e17624279f4dad7d936a0ba5b348382125d
root <root@DESKTOP-FIR4MKL>
2025-06-19 00:39:01 +0300

    minor change to avoid major docker error

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ read -rp "Bot token: " bot_token
 read -rp "Instagram login: " inst_login
 read -rp "Instagram password: " inst_password
 
-echo "BOT_TOKEN = '$bot_token'\nINST_LOGIN = '$inst_login'\nINST_PASSWORD = '$inst_password'" > .env
+echo "BOT_TOKEN = '$bot_token'\nINST_LOGIN='$inst_login'\nINST_PASSWORD='$inst_password'" > .env
 
 python3 -m venv bot-env
 source bot-env/bin/activate
