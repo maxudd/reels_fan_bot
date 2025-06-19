d8653b1ceb4d2a4119139edeb2fe51b8af541325
root <root@DESKTOP-FIR4MKL>
2025-06-19 12:32:22 +0300

    fixed .env generation

--- setup.sh
+++ setup.sh
@@ -4,7 +4,11 @@ read -rp "Bot token: " bot_token
 read -rp "Instagram login: " inst_login
 read -rp "Instagram password: " inst_password
 
-echo "BOT_TOKEN='$bot_token'\nINST_LOGIN='$inst_login'\nINST_PASSWORD='$inst_password'" > .env
+cat > .env <<EOF
+BOT_TOKEN='$bot_token'
+INST_LOGIN='$inst_login'
+INST_PASSWORD='$inst_password'
+EOF
 
 python3 -m venv bot-env
 source bot-env/bin/activate
