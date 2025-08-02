6bba1ad12f30c9182a4a37a72b99a854d81a3ecb
root <root@DESKTOP-FIR4MKL>
2025-08-03 01:00:25 +0300

    fixed comments

--- src/params.py
+++ src/params.py
@@ -1,10 +1,10 @@
 # YouTube Shorts download options
 YDL_OPTS = {
     'format': 'bestvideo[ext=mp4][height<=1080]+bestaudio[ext=m4a]/best[ext=mp4][height<=1080]',    # разрешение
-    'outtmpl': '%(title)s.%(ext)s',     # Шаблон имени файла
+    'outtmpl': '%(title)s.%(ext)s', # Шаблон имени файла
     'merge_output_format': 'mp4',   # Формат выходного файла
     'noplaylist': True,             # Не загружать плейлисты
-    'quiet': True   # HZ что это но выглядит хайпово
+    'quiet': True                   # Выводить меньше информации
 }
 
 # Video thumbnails option
